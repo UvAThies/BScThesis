d032126e1fdbdb241983e29227546ce559a158c3
Thies <git@thies.dev>
2025-04-15 22:47:28 +0200

    First iteration of design with zynq

--- BScThesis.gen/sources_1/bd/design_1/synth/design_1.v
+++ BScThesis.gen/sources_1/bd/design_1/synth/design_1.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2024.2 (win64) Build 5239630 Fri Nov 08 22:35:27 MST 2024
-//Date        : Mon Apr  7 15:48:01 2025
+//Date        : Tue Apr 15 22:44:59 2025
 //Host        : DefconeONE running 64-bit major release  (build 9200)
 //Command     : generate_target design_1.bd
 //Design      : design_1
@@ -10,21 +10,116 @@
 //--------------------------------------------------------------------------------
 `timescale 1 ps / 1 ps
 
-(* CORE_GENERATION_INFO = "design_1,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_1,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=4,numReposBlks=4,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=2,numPkgbdBlks=0,bdsource=USER,da_board_cnt=2,da_clkrst_cnt=1,synth_mode=Hierarchical}" *) (* HW_HANDOFF = "design_1.hwdef" *) 
+(* CORE_GENERATION_INFO = "design_1,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_1,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=6,numReposBlks=6,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=1,numPkgbdBlks=0,bdsource=USER,da_board_cnt=2,da_clkrst_cnt=1,da_ps7_cnt=1,synth_mode=Hierarchical}" *) (* HW_HANDOFF = "design_1.hwdef" *) 
 module design_1
-   ();
+   (DDR_addr,
+    DDR_ba,
+    DDR_cas_n,
+    DDR_ck_n,
+    DDR_ck_p,
+    DDR_cke,
+    DDR_cs_n,
+    DDR_dm,
+    DDR_dq,
+    DDR_dqs_n,
+    DDR_dqs_p,
+    DDR_odt,
+    DDR_ras_n,
+    DDR_reset_n,
+    DDR_we_n,
+    FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp,
+    FIXED_IO_mio,
+    FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb);
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR ADDR" *) (* X_INTERFACE_MODE = "Master" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME DDR, AXI_ARBITRATION_SCHEME TDM, BURST_LENGTH 8, CAN_DEBUG false, CAS_LATENCY 11, CAS_WRITE_LATENCY 11, CS_ENABLED true, DATA_MASK_ENABLED true, DATA_WIDTH 8, MEMORY_TYPE COMPONENTS, MEM_ADDR_MAP ROW_COLUMN_BANK, SLOT Single, TIMEPERIOD_PS 1250" *) inout [14:0]DDR_addr;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR BA" *) inout [2:0]DDR_ba;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR CAS_N" *) inout DDR_cas_n;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR CK_N" *) inout DDR_ck_n;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR CK_P" *) inout DDR_ck_p;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR CKE" *) inout DDR_cke;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR CS_N" *) inout DDR_cs_n;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR DM" *) inout [3:0]DDR_dm;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR DQ" *) inout [31:0]DDR_dq;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR DQS_N" *) inout [3:0]DDR_dqs_n;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR DQS_P" *) inout [3:0]DDR_dqs_p;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR ODT" *) inout DDR_odt;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR RAS_N" *) inout DDR_ras_n;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR RESET_N" *) inout DDR_reset_n;
+  (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR WE_N" *) inout DDR_we_n;
+  (* X_INTERFACE_INFO = "xilinx.com:display_processing_system7:fixedio:1.0 FIXED_IO DDR_VRN" *) (* X_INTERFACE_MODE = "Master" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME FIXED_IO, CAN_DEBUG false" *) inout FIXED_IO_ddr_vrn;
+  (* X_INTERFACE_INFO = "xilinx.com:display_processing_system7:fixedio:1.0 FIXED_IO DDR_VRP" *) inout FIXED_IO_ddr_vrp;
+  (* X_INTERFACE_INFO = "xilinx.com:display_processing_system7:fixedio:1.0 FIXED_IO MIO" *) inout [53:0]FIXED_IO_mio;
+  (* X_INTERFACE_INFO = "xilinx.com:display_processing_system7:fixedio:1.0 FIXED_IO PS_CLK" *) inout FIXED_IO_ps_clk;
+  (* X_INTERFACE_INFO = "xilinx.com:display_processing_system7:fixedio:1.0 FIXED_IO PS_PORB" *) inout FIXED_IO_ps_porb;
+  (* X_INTERFACE_INFO = "xilinx.com:display_processing_system7:fixedio:1.0 FIXED_IO PS_SRSTB" *) inout FIXED_IO_ps_srstb;
 
-  wire [63:0]xlconstant_0_dout;
-  wire [63:0]zeroes_dout;
+  wire [14:0]DDR_addr;
+  wire [2:0]DDR_ba;
+  wire DDR_cas_n;
+  wire DDR_ck_n;
+  wire DDR_ck_p;
+  wire DDR_cke;
+  wire DDR_cs_n;
+  wire [3:0]DDR_dm;
+  wire [31:0]DDR_dq;
+  wire [3:0]DDR_dqs_n;
+  wire [3:0]DDR_dqs_p;
+  wire DDR_odt;
+  wire DDR_ras_n;
+  wire DDR_reset_n;
+  wire DDR_we_n;
+  wire FIXED_IO_ddr_vrn;
+  wire FIXED_IO_ddr_vrp;
+  wire [53:0]FIXED_IO_mio;
+  wire FIXED_IO_ps_clk;
+  wire FIXED_IO_ps_porb;
+  wire FIXED_IO_ps_srstb;
+  wire [0:7]SDES_encrypt_0_output;
+  wire [17:0]ilconstant_0_dout;
+  wire [25:0]processing_system7_0_GPIO_O;
+  wire [25:0]xlconcat_0_dout;
+  wire [7:0]xlslice_0_Dout;
+  wire [9:0]xlslice_1_Dout;
 
-  design_1_xlconstant_0_0 ones
-       (.dout(xlconstant_0_dout));
-  design_1_xor_32bit_0_0 xor_32bit_0
-       (.\input ({zeroes_dout[31],zeroes_dout[30],zeroes_dout[29],zeroes_dout[28],zeroes_dout[27],zeroes_dout[26],zeroes_dout[25],zeroes_dout[24],zeroes_dout[23],zeroes_dout[22],zeroes_dout[21],zeroes_dout[20],zeroes_dout[19],zeroes_dout[18],zeroes_dout[17],zeroes_dout[16],zeroes_dout[15],zeroes_dout[14],zeroes_dout[13],zeroes_dout[12],zeroes_dout[11],zeroes_dout[10],zeroes_dout[9],zeroes_dout[8],zeroes_dout[7],zeroes_dout[6],zeroes_dout[5],zeroes_dout[4],zeroes_dout[3],zeroes_dout[2],zeroes_dout[1],zeroes_dout[0]}),
-        .key({xlconstant_0_dout[31],xlconstant_0_dout[30],xlconstant_0_dout[29],xlconstant_0_dout[28],xlconstant_0_dout[27],xlconstant_0_dout[26],xlconstant_0_dout[25],xlconstant_0_dout[24],xlconstant_0_dout[23],xlconstant_0_dout[22],xlconstant_0_dout[21],xlconstant_0_dout[20],xlconstant_0_dout[19],xlconstant_0_dout[18],xlconstant_0_dout[17],xlconstant_0_dout[16],xlconstant_0_dout[15],xlconstant_0_dout[14],xlconstant_0_dout[13],xlconstant_0_dout[12],xlconstant_0_dout[11],xlconstant_0_dout[10],xlconstant_0_dout[9],xlconstant_0_dout[8],xlconstant_0_dout[7],xlconstant_0_dout[6],xlconstant_0_dout[5],xlconstant_0_dout[4],xlconstant_0_dout[3],xlconstant_0_dout[2],xlconstant_0_dout[1],xlconstant_0_dout[0]}));
-  design_1_xor_32bit_1_0 xor_32bit_1
-       (.\input ({xlconstant_0_dout[31],xlconstant_0_dout[30],xlconstant_0_dout[29],xlconstant_0_dout[28],xlconstant_0_dout[27],xlconstant_0_dout[26],xlconstant_0_dout[25],xlconstant_0_dout[24],xlconstant_0_dout[23],xlconstant_0_dout[22],xlconstant_0_dout[21],xlconstant_0_dout[20],xlconstant_0_dout[19],xlconstant_0_dout[18],xlconstant_0_dout[17],xlconstant_0_dout[16],xlconstant_0_dout[15],xlconstant_0_dout[14],xlconstant_0_dout[13],xlconstant_0_dout[12],xlconstant_0_dout[11],xlconstant_0_dout[10],xlconstant_0_dout[9],xlconstant_0_dout[8],xlconstant_0_dout[7],xlconstant_0_dout[6],xlconstant_0_dout[5],xlconstant_0_dout[4],xlconstant_0_dout[3],xlconstant_0_dout[2],xlconstant_0_dout[1],xlconstant_0_dout[0]}),
-        .key({xlconstant_0_dout[31],xlconstant_0_dout[30],xlconstant_0_dout[29],xlconstant_0_dout[28],xlconstant_0_dout[27],xlconstant_0_dout[26],xlconstant_0_dout[25],xlconstant_0_dout[24],xlconstant_0_dout[23],xlconstant_0_dout[22],xlconstant_0_dout[21],xlconstant_0_dout[20],xlconstant_0_dout[19],xlconstant_0_dout[18],xlconstant_0_dout[17],xlconstant_0_dout[16],xlconstant_0_dout[15],xlconstant_0_dout[14],xlconstant_0_dout[13],xlconstant_0_dout[12],xlconstant_0_dout[11],xlconstant_0_dout[10],xlconstant_0_dout[9],xlconstant_0_dout[8],xlconstant_0_dout[7],xlconstant_0_dout[6],xlconstant_0_dout[5],xlconstant_0_dout[4],xlconstant_0_dout[3],xlconstant_0_dout[2],xlconstant_0_dout[1],xlconstant_0_dout[0]}));
-  design_1_xlconstant_0_2 zeroes
-       (.dout(zeroes_dout));
+  design_1_SDES_encrypt_0_0 SDES_encrypt_0
+       (.\input ({xlslice_0_Dout[7],xlslice_0_Dout[6],xlslice_0_Dout[5],xlslice_0_Dout[4],xlslice_0_Dout[3],xlslice_0_Dout[2],xlslice_0_Dout[1],xlslice_0_Dout[0]}),
+        .key({xlslice_1_Dout[9],xlslice_1_Dout[8],xlslice_1_Dout[7],xlslice_1_Dout[6],xlslice_1_Dout[5],xlslice_1_Dout[4],xlslice_1_Dout[3],xlslice_1_Dout[2],xlslice_1_Dout[1],xlslice_1_Dout[0]}),
+        .\output (SDES_encrypt_0_output));
+  assign ilconstant_0_dout = 18'h00000;
+  design_1_processing_system7_0_0 processing_system7_0
+       (.DDR_Addr(DDR_addr),
+        .DDR_BankAddr(DDR_ba),
+        .DDR_CAS_n(DDR_cas_n),
+        .DDR_CKE(DDR_cke),
+        .DDR_CS_n(DDR_cs_n),
+        .DDR_Clk(DDR_ck_p),
+        .DDR_Clk_n(DDR_ck_n),
+        .DDR_DM(DDR_dm),
+        .DDR_DQ(DDR_dq),
+        .DDR_DQS(DDR_dqs_p),
+        .DDR_DQS_n(DDR_dqs_n),
+        .DDR_DRSTB(DDR_reset_n),
+        .DDR_ODT(DDR_odt),
+        .DDR_RAS_n(DDR_ras_n),
+        .DDR_VRN(FIXED_IO_ddr_vrn),
+        .DDR_VRP(FIXED_IO_ddr_vrp),
+        .DDR_WEB(DDR_we_n),
+        .GPIO_I(xlconcat_0_dout),
+        .GPIO_O(processing_system7_0_GPIO_O),
+        .MIO(FIXED_IO_mio),
+        .PS_CLK(FIXED_IO_ps_clk),
+        .PS_PORB(FIXED_IO_ps_porb),
+        .PS_SRSTB(FIXED_IO_ps_srstb));
+  design_1_xlconcat_0_0 xlconcat_0
+       (.In0(ilconstant_0_dout),
+        .In1({SDES_encrypt_0_output[0],SDES_encrypt_0_output[1],SDES_encrypt_0_output[2],SDES_encrypt_0_output[3],SDES_encrypt_0_output[4],SDES_encrypt_0_output[5],SDES_encrypt_0_output[6],SDES_encrypt_0_output[7]}),
+        .dout(xlconcat_0_dout));
+  design_1_xlslice_0_0 xlslice_0
+       (.Din(processing_system7_0_GPIO_O[24:0]),
+        .Dout(xlslice_0_Dout));
+  design_1_xlslice_0_1 xlslice_1
+       (.Din(processing_system7_0_GPIO_O[24:0]),
+        .Dout(xlslice_1_Dout));
 endmodule
